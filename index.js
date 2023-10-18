e8c72409b7e004eaff46e328ff0f70eb66c9e053
masud520 <masud.abdi@student.moringaschool.com>
2023-10-18 11:19:53 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usage:
+  const names = ["Alice", "Bob", "Charlie"];
+  const event = "birthday";
+  const countdownNumber = 5;
+  
+  const thankYouMessages = writeCards(names, event);
+  console.log(thankYouMessages);
+  
+  countDown(countdownNumber);
+  
